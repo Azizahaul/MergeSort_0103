74a428f260ae1f7b69f8c3a73b48e839e18e902b
Azizahaul <azizahauliarhamid@gmail.com>
2025-04-17 18:32:10 +0700

    step 4.d.i.1

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -25,6 +25,7 @@ int n;
         while (i <= mid  && j <= high) // step 4d
         {
             if(arr[i] <= arr[j]){ //step 4.d.i
+                b[k] = arr[i]; // step 4.d.i.1
 
             }
         }
