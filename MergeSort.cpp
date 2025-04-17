9392d4270d9cd358083a793aa5f3ed513b238564
Azizahaul <azizahauliarhamid@gmail.com>
2025-04-17 18:10:08 +0700

    masukkan panjang element

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -3,3 +3,11 @@ using namespace std;
 
 int arr[20], B[20]; // Array utama dan array bantu
 int n;
+
+void input(){
+    while(true){
+        cout << "Masukkan Panjang Element Array: ";
+        cin >> n;
+        
+    }
+}
